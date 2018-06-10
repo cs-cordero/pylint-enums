c9414b92004627c1abdb4f56310bb85e8d296a73
Christopher Cordero <chris.s.cordero@gmail.com>
2018-06-09 23:21:25 -0400

    initial implementation

---
+++ checker.py
@@ -0,0 +1,103 @@
+import astroid
+
+from pylint.checkers import BaseChecker
+from pylint.interfaces import IAstroidChecker
+
+class EnumChecker(BaseChecker):
+    __implements__ = IAstroidChecker
+
+    name = 'enum-implementation'
+    priority = -1
+    msgs = {
+        'W5501': (
+            'Enums must have their value attribute annotated',
+            'pylint-enums-no-annotated-value',
+            'All enums should have their value attribute annotated.'
+        ),
+        'W5502': (
+            'Enums must not assign a value to their \'value\' attribute',
+            'pylint-enums-no-assignment-to-value',
+            'All enums should not assign a value to their value attribute.'
+        ),
+        'W5502': (
+            'Enums must implement their __str__ method',
+            'pylint-enums-no-str-method',
+            'All enums implement a __str__ method.'
+        ),
+    }
+
+    def __init__(self, linter=None):
+        super().__init__(linter)
+        self.enum_imported = False
+
+    def visit_module(self, node):
+        self.enum_imported = False
+
+    def visit_import(self, node):
+        """
+        Checks whether an enum module has been imported
+            i.e.,  import enum
+        """
+        if self.enum_imported:
+            return
+
+        for module_name, as_name in node.names:
+            if module_name == 'enum':
+                self.enum_imported = True
+                return
+
+    def visit_importfrom(self, node):
+        """
+        Checks whether an Enum class has been imported with the "from" convention
+        from an enum module.
+            i.e.,  from enum import Enum
+        """
+        if self.enum_imported or not node.modname == 'enum':
+            return
+
+        for module_name, as_name in node.names:
+            if module_name in ('Enum', '*'):
+                self.enum_imported = True
+                return
+
+    def visit_classdef(self, node):
+        """
+        Checks whether an Enum subclass has a value annotation and a __str__ method
+        """
+        if not self.enum_imported:
+            return
+
+        for base_class in node.bases:
+            if isinstance(base_class, astroid.node_classes.Attribute):
+                base_class_name = base_class.attrname
+            elif isinstance(base_class, astroid.node_classes.Name):
+                base_class_name = base_class.name
+            else:
+                continue
+
+            if base_class_name == 'Enum':
+                break
+        else:
+            # did not find a base class that extended Enum
+            return
+
+        value_annotated = False
+        for child_node in node.get_children():
+            if not isinstance(child_node, astroid.node_classes.AnnAssign):
+                continue
+
+            if child_node.target == 'value':
+                if child_node.value is not None:
+                    self.add_message('pylint-enums-no-assignment-to-value', node=node)
+                    break
+                value_annotated = True
+
+        if not value_annotated:
+            self.add_message('pylint-enums-no-annotated-value', node=node)
+
+        if not any(method.name == '__str__' for method in node.mymethods()):
+            self.add_message('pylint-enums-no-str-method', node=node)
+
+
+def register(linter):
+    linter.register_checker(EnumChecker(linter))
